1d71fac740edf807ada07ea4183ab2628814d800
Emil Kriukov <77118598+mcrewcow@users.noreply.github.com>
2023-03-25 14:53:07 -0400

    Create cellchat.R

---
+++ cellchat.R
@@ -0,0 +1,19 @@
+library(CellChat)
+
+lyd_RGC_KO <- subset(lyd_RGC, subset = class_condition == 'RGCs-DFcKO')
+lyd_RGC_WT <- subset(lyd_RGC, subset = class_condition == 'RGCs-WT')
+
+cellchat <- createCellChat(object = lyd_RGC_WT, group.by = "annotation")
+CellChatDB <- CellChatDB.mouse
+CellChatDB.use <- CellChatDB
+cellchat@DB <- CellChatDB.use
+cellchat <- subsetData(cellchat)
+cellchat <- identifyOverExpressedGenes(cellchat)
+cellchat <- identifyOverExpressedInteractions(cellchat)
+cellchat <- projectData(cellchat, PPI.mouse)
+cellchat <- computeCommunProb(cellchat, type = "truncatedMean", trim = 0.1, raw.use = FALSE, population.size = TRUE)
+cellchat <- filterCommunication(cellchat, min.cells = 3)
+cellchat <- computeCommunProbPathway(cellchat)
+cellchat <- aggregateNet(cellchat)
+cellchat <- netAnalysis_computeCentrality(cellchat, slot.name = "netP")
+cellchat_lydia_WT <- cellchat
