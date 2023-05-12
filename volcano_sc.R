883c2b4f43b0082de94eb9825fa840acd2a800da
Emil Kriukov <77118598+mcrewcow@users.noreply.github.com>
2023-05-12 14:17:57 -0400

    Update volcano_sc.R

--- volcano_sc.R
+++ volcano_sc.R
@@ -6,3 +6,12 @@ D59fetalS.markers <- FindAllMarkers(lyd_RGC, only.pos = FALSE, thresh.use = NONE
 BiocManager::install('EnhancedVolcano')
 library(EnhancedVolcano)
 
+EnhancedVolcano(bulk,
+                lab = bulk$gene_name,
+                x = 'log2FoldChange',
+                y = 'padj',
+                pCutoff = 5e-2, legendPosition = 'right',
+                legendLabSize = 12,
+                legendIconSize = 4.0,
+                drawConnectors = TRUE,
+                widthConnectors = 0.75, max.overlaps=20) + xlim(-10,10) #top-30
