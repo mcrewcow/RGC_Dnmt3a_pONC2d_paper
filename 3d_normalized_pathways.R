11b430a5715d84db2d2ebcc239877bd4483dc1d5
Emil Kriukov <77118598+mcrewcow@users.noreply.github.com>
2023-05-25 23:22:00 -0400

    Create 3d_normalized_pathways.R

---
+++ 3d_normalized_pathways.R
@@ -0,0 +1,70 @@
+axdev <- aggregate(lyd_RGC$GOBP_AXON_DEVELOPMENT, list(lyd_RGC$annotation, lyd_RGC$background), FUN = mean)
+axdev$axdev <- fun_range(x = axdev$x)
+opnm <- aggregate(lyd_RGC$GOBP_OPTIC_NERVE_MORPHOGENESIS, list(lyd_RGC$annotation, lyd_RGC$background), FUN = mean)
+opnm$opnm <- fun_range(x = opnm$x)
+regax <- aggregate(lyd_RGC$GOBP_REGULATION_OF_AXONOGENESIS, list(lyd_RGC$annotation, lyd_RGC$background), FUN = mean)
+regax$regax <- fun_range(x = regax$x)
+dende <- aggregate(lyd_RGC$GOBP_DENDRITE_DEVELOPMENT, list(lyd_RGC$annotation, lyd_RGC$background), FUN = mean)
+dende$dende <- fun_range(x = dende$x)
+
+neude <- aggregate(lyd_RGC$GOBP_NEURON_DEATH, list(lyd_RGC$annotation, lyd_RGC$background), FUN = mean)
+neude$neude <- fun_range(x = neude$x) 
+resax <- aggregate(lyd_RGC$GOBP_RESPONSE_TO_AXON_INJURY, list(lyd_RGC$annotation, lyd_RGC$background), FUN = mean)
+resax$resax <- fun_range(x = resax$x) 
+
+acinf <- aggregate(lyd_RGC$GOBP_ACUTE_INFLAMMATORY_RESPONSE, list(lyd_RGC$annotation, lyd_RGC$background), FUN = mean)
+acinf$acinf <- fun_range(x = acinf$x) 
+cytpr <- aggregate(lyd_RGC$GOBP_CYTOKINE_PRODUCTION_INVOLVED_IN_INFLAMMATORY_RESPONSE, list(lyd_RGC$annotation, lyd_RGC$background), FUN = mean)
+cytpr$cytpr <- fun_range(x = cytpr$x) 
+infco <- aggregate(lyd_RGC$GOBP_INFLAMMASOME_COMPLEX_ASSEMBLY, list(lyd_RGC$annotation, lyd_RGC$background), FUN = mean)
+infco$infco <- fun_range(x = infco$x) 
+infre <- aggregate(lyd_RGC$GOBP_INFLAMMATORY_RESPONSE, list(lyd_RGC$annotation, lyd_RGC$background), FUN = mean)
+infre$infre <- fun_range(x = infre$x) 
+neure <- aggregate(lyd_RGC$GOBP_NEUROINFLAMMATORY_RESPONSE, list(lyd_RGC$annotation, lyd_RGC$background), FUN = mean)
+neure$neure <- fun_range(x = neure$x) 
+
+final <- dende
+final$regax <- regax$regax
+final$opnm <- opnm$opnm
+final$axdev <- axdev$axdev
+final$neude <- neude$neude
+final$resax <- resax$resax
+final$acinf <- acinf$acinf
+final$cytpr <- cytpr$cytpr
+final$infco <- infco$infco
+final$infre <- infre$infre
+final$neure <- neure$neure
+
+final$regenfinal <- final$dende + final$regax + final$opnm + final$axdev
+final$injury <- final$resax + final$neude
+final$inflam <- final$acinf + final$cytpr + final$infco + final$infre + final$neure
+final$regenfinal_new <- fun_range(x = final$regenfinal) 
+final$injury_new <- fun_range(x = final$injury) 
+final$inflam_new <- fun_range(x = final$inflam) 
+
+testnew <- final %>% select(Group.1, regenfinal_new, injury_new, inflam_new, Group.2) %>%
+  pivot_longer(., cols = c(regenfinal_new, injury_new, inflam_new), names_to = "Var", values_to = "Val")
+
+ggplot(testnew, aes(x = reorder(Group.1, +Val), y = Val, fill = Group.2, color = Group.2, group = Group.2)) +
+  geom_point() + geom_line() + theme_bw() + coord_flip() + facet_wrap(~Var)
+
+final_ko <- final[which(final$Group.2=='DFcKO'),]
+final_wt <- final[which(final$Group.2=='WT'),]
+final_ko$delta_injury_new <- final_ko$injury_new - final_wt$injury_new
+final_ko$delta_infl_new <- final_ko$inflam_new - final_wt$inflam_new
+final_ko$delta_regen_new <- final_ko$regenfinal_new - final_wt$regenfinal_new
+
+library(plotly)
+
+fig <- plot_ly(final_ko, x = ~delta_injury_new, y = ~delta_infl_new, z = ~delta_regen_new,  color = ~Group.1, text = final_ko$Group.1)  %>% 
+  add_markers() %>% 
+  layout(scene = list(xaxis = list(title = "Δ inflammation", zerolinewidth=4, zerolinecolor='black'),
+                      yaxis = list(title = "Δ regeneration",zerolinewidth=4, zerolinecolor='black'),
+                      zaxis = list(title = "Δ injury",zerolinewidth=4, zerolinecolor='black')))
+fig
+
+htmlwidgets::saveWidget(
+widget = fig, #the plotly object
+file = "C://Users/Emil/Downloads/figure.html", #the path & file name
+selfcontained = TRUE #creates a single html file
+)
